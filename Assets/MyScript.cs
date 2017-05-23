19d22ca1526dccdd06244c0b7c34fa8db60a5e53
Michael Wade Rhodehamel <rhodeham@hotmail.com>
2017-05-23 03:32:09 -0700

    Made change to MyScript to test SourceTree and GitHub

--- Assets/MyScript.cs
+++ Assets/MyScript.cs
@@ -7,6 +7,7 @@ public class MyScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		print("You have found the sausages");
+		print("Changed this line to test SourceTree and GitHub");
 	}
 	
 	// Update is called once per frame

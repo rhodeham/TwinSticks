145189346bb6dea76050c629c3120273b69a0785
Michael Wade Rhodehamel <rhodeham@hotmail.com>
2017-05-24 01:40:29 -0700

    Final State of TwinSticks Game.  I did not completely finished tutorial, so instructor version of end state differs.

---
+++ Assets/Game/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class GameManager : MonoBehaviour {
+
+	public bool recording = true;
+
+	private bool isPaused = false;
+	private float initialFixedDeltaTime;
+
+	void Start () {
+		initialFixedDeltaTime = Time.fixedDeltaTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (CrossPlatformInputManager.GetButton("Fire1")) { 
+			recording = false;
+		} else {
+			recording = true;
+		}
+
+		if (Input.GetKeyDown (KeyCode.P)) {
+			if (!isPaused) {
+				PauseGame();
+			} else {
+				ResumeGame ();
+			}
+		}
+
+		//print ("Update");
+	}
+
+	void PauseGame () {
+		Time.timeScale = 0;
+		Time.fixedDeltaTime = 0;
+		isPaused = true;
+	}
+
+	void ResumeGame () {
+		Time.timeScale = 1;
+		Time.fixedDeltaTime = initialFixedDeltaTime;
+		isPaused = false;
+	}
+}
---
+++ Assets/Game/ReplaySystem.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReplaySystem : MonoBehaviour {
+
+	private const int bufferFrames = 300;
+	private MyKeyFrame[] keyFrames = new MyKeyFrame[bufferFrames];
+	private Rigidbody rigidBody;
+	private GameManager gameManager;
+	private int numberFramesRecorded = 0;
+	private int numberFramesPlayed = 0;
+	private bool startRecord = false;
+
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody> ();
+		gameManager = GameObject.FindObjectOfType<GameManager>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if(!startRecord && gameManager.recording) {
+			numberFramesRecorded = 0;
+			numberFramesPlayed = 0;
+		}
+
+		if (gameManager.recording) {
+			Record();
+			startRecord = true;
+		} else {
+			PlayBack();
+			startRecord = false;
+		}
+
+	}
+
+	void PlayBack () {
+		rigidBody.isKinematic = true;
+		int startFrame = Mathf.Max(0, numberFramesRecorded - bufferFrames) % bufferFrames;
+		//print ("Start Frame " + startFrame);
+		//print ("Number Frames Played " + numberFramesPlayed);
+		int frame = (startFrame + numberFramesPlayed) % Mathf.Min(Mathf.Max(numberFramesRecorded,1), bufferFrames);
+		//print ("Reading Frame " + frame);
+		transform.position = keyFrames[frame].position;
+		transform.rotation = keyFrames[frame].rotation;
+		numberFramesPlayed++;
+
+	}
+
+	void Record ()
+	{
+		int frame = numberFramesRecorded % bufferFrames;
+		float time = Time.time;
+		rigidBody.isKinematic = false;
+		//print ("Writing Frame " + frame);
+		keyFrames [frame] = new MyKeyFrame (time, transform.position, transform.rotation);
+		numberFramesRecorded++;
+	}
+}
+
+/// <summary>
+/// A structure for storting time, position and rotation.
+/// </summary>
+public struct MyKeyFrame {
+
+	public float frameTime;
+	public Vector3 position;
+	public Quaternion rotation;
+
+	public MyKeyFrame (float aTime, Vector3 aPos, Quaternion aRot) {
+		frameTime = aTime;
+		position = aPos;
+		rotation = aRot;
+	}
+
+}
